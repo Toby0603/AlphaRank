e2a2ee0e8e81d7fe60e70c31db9e8dce58d24e6f
Toby0603 <tobystrickland54@gmail.com>
2026-03-30 16:43:22 +0100

    Update update_results_catboost.py

--- update_results_catboost.py
+++ update_results_catboost.py
@@ -1,5 +1,5 @@
 from backend_multi_model_utils import BASE_DIR, run_variant
 
 if __name__ == "__main__":
-    run_variant("catboost", "base" BASE_DIR / "results_catboost_base.csv")
-    run_variant("catboost", "interactions" BASE_DIR / "results_catboost_interactions.csv")
+    run_variant("catboost", "base", BASE_DIR / "results_catboost_base.csv")
+    run_variant("catboost", "interactions", BASE_DIR / "results_catboost_interactions.csv")
