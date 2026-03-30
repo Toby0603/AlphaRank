db8cfbeeaf408477c2cf352e49ad9da24f118268
Toby0603 <tobystrickland54@gmail.com>
2026-03-30 15:28:23 +0100

    Update update_results_catboost.py

--- update_results_catboost.py
+++ update_results_catboost.py
@@ -1,4 +1,5 @@
 from backend_multi_model_utils import BASE_DIR, run_model
 
 if __name__ == "__main__":
-    run_model("catboost", BASE_DIR / "results_catboost.csv")
+    run_varinat("catboost", "base" BASE_DIR / "results_catboost_base.csv")
+    run_varinat("catboost", "interactions" BASE_DIR / "results_catboost_interactions.csv")
